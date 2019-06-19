bd3b599436ceb6c77b290b1951cafd6757dd6d42
sophie-harrington <sophie.harrington@jic.ac.uk>
2019-06-19 10:23:19 +0100

    Remove email
    Removed email address from the .sh file

--- Circos_Plot_Scripts/Circos.sh
+++ Circos_Plot_Scripts/Circos.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-#SBATCH -p RG-Cristobal-Uauy # partition (queue)
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --mem=5000
 #SBATCH -J Circos
 #SBATCH -o Circos.%N.%j.out
 #SBATCH -e Circos.%N.%j.err
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=sophie.harrington@jic.ac.uk
 
 source perl-5.22.1
 source circos-0.69.3
